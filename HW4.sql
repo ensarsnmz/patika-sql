13908252ee2b2efd4b0c591f93c93203027eeef0
ensarsnmz <53300933+ensarsnmz@users.noreply.github.com>
2022-12-06 21:11:09 +0300

    Add files via upload

---
+++ HW4.sql
@@ -0,0 +1,17 @@
+-- q1
+SELECT DISTINCT replacement_cost FROM film;
+
+-- q2
+SELECT COUNT(distinct replacement_cost) FROM film;
+
+-- q3
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+-- q4
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
+
+-- q5
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%R';
