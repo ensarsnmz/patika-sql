c5baa24f1f2123d642356c83827a71030e0e7400
ensarsnmz <53300933+ensarsnmz@users.noreply.github.com>
2022-12-06 18:33:50 +0300

    Add files via upload

--- HW3.sql
+++ HW3.sql
@@ -10,6 +10,6 @@ WHERE country LIKE '%_____n';
 SELECT title FROM film
 WHERE title ILIKE '%t%t%t%t%';
 
--- q 4
+-- q4
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
